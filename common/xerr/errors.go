c258cfcff8bcf3053ff2471247bb692fd4607153
xiaojin <jx_xiaojin@aliyun.com>
2022-02-22 18:30:40 +0800

    新增错误包

---
+++ common/xerr/errors.go
@@ -0,0 +1,32 @@
+package xerr
+
+import "fmt"
+
+type CodeError struct {
+	errCode uint32
+	errMsg  string
+}
+
+func (e *CodeError) GetErrCode() uint32 {
+	return e.errCode
+}
+
+func (e *CodeError) GetErrMsg() string {
+	return e.errMsg
+}
+
+func (e *CodeError) Error() string {
+	return fmt.Sprintf("ErrCode:%d，ErrMsg:%s", e.errCode, e.errMsg)
+}
+
+func NewErrCodeMsg(errCode uint32, errMsg string) *CodeError {
+	return &CodeError{errCode: errCode, errMsg: errMsg}
+}
+
+func NewErrCode(errCode uint32) *CodeError {
+	return &CodeError{errCode: errCode}
+}
+
+func NewErrMsg(errMsg string) *CodeError {
+	return &CodeError{errMsg: errMsg}
+}
